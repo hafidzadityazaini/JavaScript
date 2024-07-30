c1845c04410d3a22cc60e2cdf71c80ef9eb76638
hafidzadityazaini <160695414+hafidzadityazaini@users.noreply.github.com>
2024-07-30 11:00:22 +0700

    JavaScript
    JavaScript

--- java.js
+++ java.js
@@ -1,2 +1,14 @@
-console.log("Yazid");
-console.log("Habibi");
+// no 1
+var a;
+// no 2
+var a = null
+console.log(a);
+// no 3
+const name = "Hafidz"
+var lastname = "Aditya"
+var umur = "16 tahun"
+console.log(name +' '+ lastname +" " + umur);
+// no 4
+var alas = 5
+var tinggi = 7
+console.log(1/2 * alas * tinggi);
