af3672a4c2ffafc5d799b26ffe3b44ed3128a62c
hafidzadityazaini <160695414+hafidzadityazaini@users.noreply.github.com>
2024-08-06 11:16:19 +0700

    java5.js
    java5.js

---
+++ java5.js
@@ -0,0 +1,59 @@
+//soal 1 start
+let hari = "senin";
+
+switch (hari) {
+  case "senin":
+    console.log("Weekday");
+    break;
+  case "selasa":
+    console.log("Weekday");
+    break;
+  case "rabu":
+    console.log("Weekday");
+    break;
+  case "kamis":
+    console.log("Weekday");
+    break;
+  case "jumat":
+    console.log("Weekday");
+    break;
+  case "sabtu":
+    console.log("Weekend");
+    break;
+  case "minggu":
+    console.log("Weekend");
+    break;
+}
+//soal 1 clear
+
+//soal 2 start
+for (let i = 1; i <= 15; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+// soal 2 clear
+
+//soal 3 start
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+//soal 3 clear
+
+//soal 4 start
+for (let i = 1; i <= 15; i++) {
+  if (i % 3 === 0) {
+    console.log("multipleofthree");
+  } else {
+    console.log(i);
+  }
+}
+//soal 4 clear
