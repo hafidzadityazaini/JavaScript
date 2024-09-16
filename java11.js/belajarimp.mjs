40f10dc9c82dd8f1781f9433f584a63e26d5aaa6
hafidzadityazaini <160695414+hafidzadityazaini@users.noreply.github.com>
2024-09-16 10:13:39 +0700

    java11.js
    Banyak

---
+++ java11.js/belajarimp.mjs
@@ -0,0 +1,3 @@
+import { myfunction } from "./belajarexp.mjs";
+
+myfunction();
---
+++ java11.js/main.mjs
@@ -0,0 +1,4 @@
+import {pi, add, subtract} from "./math.mjs";
+console.log(pi);
+console.log(add(3,5));
+console.log(subtract(7,3));
