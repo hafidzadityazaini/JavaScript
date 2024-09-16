9a9b9de0b5e659be797b835bb86835eb758ac540
hafidzadityazaini <160695414+hafidzadityazaini@users.noreply.github.com>
2024-09-16 10:20:01 +0700

    java12.js
    Susah

---
+++ java12.js/latihan12.js
@@ -0,0 +1,56 @@
+/*function mandi() {
+  console.log("mandi");
+}
+function sarapan(callback) {
+  console.log("Sarapan tertunda 3 detik");
+  setTimeout(callback, 3000);
+}
+function berangkatSekolah() {
+  console.log("berangkat sekolah");
+}
+mandi();
+sarapan(berangkatSekolah); */
+
+//soal 1 start
+
+// function printMessages() {
+//   console.log("mulai");
+//   setTimeout(() => console.log("selesai"), 1000);
+// }
+
+// printMessages();
+
+//soal 1 clear
+
+//soal 2 start
+// function printEverySecond() {
+//   let counter = 0;
+//   let interval = setInterval(() => {
+//     console.log("Hello World");
+//     counter++;
+//     if (counter === 5) {
+//       clearInterval(interval);
+//     }
+//   }, 1000);
+// }
+
+// printEverySecond();
+
+//soal 2 clear
+
+//soal 3 start
+
+// function greet(name, callback) {
+//     console.log(`Hello ${name}`);
+//     setTimeout(callback, 1000);
+// }
+
+// function sayGoodbye() {
+//     console.log("Goodbye");
+// }
+
+// greet("Adit", sayGoodbye)
+
+//soal 3 clear
+
+//soal 4 start
---
+++ java12.js/latihanskilvull.js
@@ -0,0 +1,12 @@
+/*function mandi() {
+  console.log("mandi");
+}
+function sarapan(callback) {
+  console.log("Sarapan tertunda 3 detik");
+  setTimeout(callback, 3000);
+}
+function berangkatSekolah() {
+  console.log("berangkat sekolah");
+}
+mandi();
+sarapan(berangkatSekolah); */
