56e0b5d80622ec8731a33c90de585e8aeadfe64c
hafidzadityazaini <160695414+hafidzadityazaini@users.noreply.github.com>
2024-10-09 13:36:35 +0700

    Tugas16

--- java12.js/soal5.js
+++
@@ -1,8 +0,0 @@
-//soal 5 start
-function delayedMessage(message, delay) {
-    setTimeout(() => console.log(message), delay);
-  }
-
-  console.log(delayedMessage("Hello after delay", 2000));
-
-  //soal 5 clear
